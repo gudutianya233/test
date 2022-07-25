4f160317b1b1131992ab24e8234e602b1f0ced0b
孤独天涯 <gudutianya233@qq.com>
2022-07-25 09:54:16 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <div>
+    <!--使用 router-link 组件进行导航 -->
+    <!--通过传递 `to` 来指定链接 -->
+    <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+      <router-link  :to="{name:'login'}">跳转a</router-link>
+      <router-link  style="margin-left:200px" :to="{name:'main'}">跳转b</router-link>
+    </div>
+    <hr />
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped></style>
---
+++ src/components/b.vue
@@ -0,0 +1,7 @@
+<template>
+<p>bbb</p>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="less">
+</style>
